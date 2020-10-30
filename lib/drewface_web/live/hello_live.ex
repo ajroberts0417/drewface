e4bd8c5888ffac32efcb681fdbf042b6004ac3b9
ajroberts0417 <ajroberts0417@gmail.com>
2020-10-29 18:15:06 -0700

    create circles

--- lib/drewface_web/live/hello_live.ex
+++ lib/drewface_web/live/hello_live.ex
@@ -1,5 +1,6 @@
 defmodule DrewfaceWeb.HelloLive do
   use DrewfaceWeb, :live_view
+  require Logger
 
   @impl true
   def mount(_params, _session, socket) do
@@ -8,9 +9,62 @@ defmodule DrewfaceWeb.HelloLive do
   end
 
   @impl true
-  def handle_event("circle_click", _value, socket) do
-    {:noreply, assign(socket, :circle_color, change_color(socket.assigns.circle_color))}
+  def handle_event(
+        "circle_click",
+        %{"circleid" => id, "circlecreate" => "true", "circlecolor" => color},
+        socket
+      ) do
+    id = String.to_integer(id)
+    next_id = id + 1
+    Logger.debug("CREATE: #{inspect(id)}, #{inspect(color)}")
+
+    {:noreply,
+     update(socket, :circles, fn circles ->
+       circles
+       |> Map.put(id, %{:id => id, :color => change_color(color), :create => false})
+       |> Map.put(next_id, %{:id => next_id, :color => change_color(), :create => true})
+     end)}
   end
+
+  @impl true
+  def handle_event(
+        "circle_click",
+        %{"circleid" => id, "circlecreate" => "false", "circlecolor" => color},
+        socket
+      ) do
+    id = String.to_integer(id)
+    Logger.debug("NO CREATE #{inspect(id)}")
+
+    {:noreply,
+     update(socket, :circles, fn circles ->
+       put_in(circles, [id, :color], change_color(color))
+     end)}
+  end
+
+  # @impl true
+  # def handle_event("circle1_click", _value, socket) do
+  #   {:noreply, assign(socket, :circle1_color, change_color())}
+  # end
+
+  # @impl true
+  # def handle_event("circle2_click", _value, socket) do
+  #   {:noreply, assign(socket, :circle2_color, change_color())}
+  # end
+
+  # @impl true
+  # def handle_event("circle3_click", _value, socket) do
+  #   {:noreply, assign(socket, :circle3_color, change_color())}
+  # end
+
+  # @impl true
+  # def handle_event("circle4_click", _value, socket) do
+  #   {:noreply, assign(socket, :circle4_color, change_color())}
+  # end
+
+  # @impl true
+  # def handle_event("circle5_click", _value, socket) do
+  #   {:noreply, assign(socket, :circle5_color, change_color())}
+  # end
 
   @impl true
   def handle_params(params, _url, socket) do
@@ -20,14 +74,34 @@ defmodule DrewfaceWeb.HelloLive do
   defp apply_action(socket, :index, _params) do
     socket
     |> assign(:page_title, "Hello")
-    |> assign(:circle_color, "bg-baby-blue")
+    |> assign(:circles, %{1 => %{:id => 1, :color => change_color(), :create => true}})
   end
 
-  defp change_color("bg-pink") do
-    "bg-baby-blue"
+  defp change_color() do
+    Enum.random([
+      "bg-orange-400",
+      "bg-yellow",
+      "bg-green-400",
+      "bg-indigo-500",
+      "bg-purple-500",
+      "bg-pink",
+      "bg-baby-blue",
+      "bg-mint"
+    ])
   end
 
-  defp change_color("bg-baby-blue") do
-    "bg-pink"
+  defp change_color(color) do
+    [
+      "bg-orange-400",
+      "bg-yellow",
+      "bg-green-400",
+      "bg-indigo-500",
+      "bg-purple-500",
+      "bg-pink",
+      "bg-baby-blue",
+      "bg-mint"
+    ]
+    |> List.delete(color)
+    |> Enum.random()
   end
 end
